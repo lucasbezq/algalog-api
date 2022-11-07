38014605683751f1b63bbe06b7ee5142b9584acb
Lucas Ezequiel <lucasbarrosez@gmail.com>
2022-11-07 10:31:55 -0300

    feat: mapping the classes

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,15 +1,26 @@
 package com.algaworks.algalog.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
-    private String email;
-    private String telefone;
 
+    private String nome;
+
+    private String email;
+
+    @Column(name = "celular")
+    private String telefone;
 }
